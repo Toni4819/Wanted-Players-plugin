41cb7fc6fdeb8c8ccf3e7e4af9a335fd137149fe
Toni_4819 <capber.antoine@gmail.com>
2026-02-07 20:43:00 +0100

    fixed custom killstreak tracking placeholder not working money sources are now configurable by entity type

--- src/main/java/org/toni_4819/wantedPlayers/listeners/KillListener.java
+++ src/main/java/org/toni_4819/wantedPlayers/listeners/KillListener.java
@@ -1,4 +1,3 @@
-/* Decompiler 27ms, total 165ms, lines 66 */
 package org.toni_4819.wantedPlayers.listeners;
 
 import java.util.Random;
@@ -7,10 +6,11 @@ import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.toni_4819.wantedPlayers.WantedPlayers;
 import org.toni_4819.wantedPlayers.utils.WantedManager;
 
@@ -23,44 +23,56 @@ public class KillListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerKill(PlayerDeathEvent event) {
-        Player var3 = event.getEntity().getKiller();
-        if (var3 instanceof Player) {
-            Player killer = var3;
-            String ksStr = PlaceholderAPI.setPlaceholders(var3, "%pvpstats_killstreak%");
-            int killstreak = 0;
+    public void onEntityKill(EntityDeathEvent event) {
+        Player killer = event.getEntity().getKiller();
+        if (killer == null) return;
 
-            try {
-                killstreak = Integer.parseInt(ksStr);
-            } catch (NumberFormatException var18) {
+        // Récupération du killstreak via PlaceholderAPI
+        String ksStr = PlaceholderAPI.setPlaceholders(killer, plugin.getConfig().getString("killstreak-placeholder", "%pvpstats_killstreak%"));
+        int killstreak = 0;
+        try {
+            killstreak = Integer.parseInt(ksStr);
+        } catch (NumberFormatException ignored) {}
+
+        int wantedLevel = WantedManager.getWantedLevel(killstreak);
+        if (wantedLevel <= 0) return;
+
+        // Base reward
+        int min = plugin.getConfig().getInt("rewards.ranges." + wantedLevel + ".min");
+        int max = plugin.getConfig().getInt("rewards.ranges." + wantedLevel + ".max");
+        int baseReward = min + random.nextInt(max - min + 1);
+
+        // Multipliers
+        double wantedMult = plugin.getConfig().getDouble("rewards.multipliers." + wantedLevel, 1.0D);
+        double groupMult = 1.0D;
+        String groupName = "default";
+
+        try {
+            LuckPerms lp = LuckPermsProvider.get();
+            User user = lp.getUserManager().getUser(killer.getUniqueId());
+            if (user != null && !user.getPrimaryGroup().isEmpty()) {
+                groupName = user.getPrimaryGroup();
+                groupMult = plugin.getConfig().getDouble("rewards.group-multipliers." + groupName,
+                        plugin.getConfig().getDouble("rewards.group-multipliers.default", 1.0D));
             }
-
-            int wantedLevel = WantedManager.getWantedLevel(killstreak);
-            if (wantedLevel > 0) {
-                int min = this.plugin.getConfig().getInt("rewards.ranges." + wantedLevel + ".min");
-                int max = this.plugin.getConfig().getInt("rewards.ranges." + wantedLevel + ".max");
-                int baseReward = min + this.random.nextInt(max - min + 1);
-                double wantedMult = this.plugin.getConfig().getDouble("rewards.multipliers." + wantedLevel, 1.0D);
-                double groupMult = 1.0D;
-                String groupName = "default";
-
-                try {
-                    LuckPerms lp = LuckPermsProvider.get();
-                    User user = lp.getUserManager().getUser(killer.getUniqueId());
-                    if (user != null && !user.getPrimaryGroup().isEmpty()) {
-                        groupName = user.getPrimaryGroup();
-                        groupMult = this.plugin.getConfig().getDouble("rewards.group-multipliers." + groupName, this.plugin.getConfig().getDouble("rewards.group-multipliers.default", 1.0D));
-                    }
-                } catch (Exception var17) {
-                    this.plugin.getLogger().warning("Could not retrieve LuckPerms group for " + killer.getName());
-                }
-
-                int reward = (int)((double)baseReward * wantedMult * groupMult);
-                WantedPlayers.getEconomy().depositPlayer(killer, (double)reward);
-                String stars = WantedManager.getStars(wantedLevel);
-                String msg = this.plugin.getConfig().getString("messages.kill-reward").replace("%amount%", String.valueOf(reward)).replace("%stars%", stars).replace("%group%", groupName);
-                killer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-            }
+        } catch (Exception e) {
+            plugin.getLogger().warning("Could not retrieve LuckPerms group for " + killer.getName());
         }
+
+        // Mob multiplier
+        EntityType type = event.getEntityType();
+        double sourceMult = plugin.getConfig().getDouble("reward-sources." + type.name().toLowerCase(), 1.0D);
+
+        // Final reward
+        int reward = (int) (baseReward * wantedMult * groupMult * sourceMult);
+        WantedPlayers.getEconomy().depositPlayer(killer, reward);
+
+        // Message
+        String stars = WantedManager.getStars(wantedLevel);
+        String msg = plugin.getConfig().getString("messages.kill-reward")
+                .replace("%amount%", String.valueOf(reward))
+                .replace("%stars%", stars)
+                .replace("%group%", groupName);
+        killer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
     }
 }
